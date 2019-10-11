23bfa3d0538207d96c94ad107f154a8a2836bf3f
bomma.anil <anil.bommaa@gmail.com>
2019-10-11 15:56:33 -0500

    - added unit test case - added vowel finder logic

---
+++ test/main.test.js
@@ -0,0 +1,10 @@
+QUnit.module('MAIN MODULE', {})  // group all these tests together
+
+QUnit.test('TEST vowelCount', assert => {
+    assert.equal(vowelCount(""), 0, 'when no input is passed then count is 0');
+    assert.equal(vowelCount("zzz"), 0, 'when input passed without vowel then count is 0');
+    assert.equal(vowelCount("hello"), 2, 'when input passed with vowel then count is 2');
+    assert.equal(vowelCount("aeiou"), 5, 'when input passed with all vowels in string, 5');
+    assert.equal(vowelCount("12345"), 0, 'when number input is passed then return 0');
+    assert.equal(vowelCount("!@#$%^&"), 0, 'when special charater input is passed then return 0');
+})
